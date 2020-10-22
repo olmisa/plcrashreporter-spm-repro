73b48ab74c49716c9c2b0f199e953f550944a600
Bruno Garcia <bruno@brunogarcia.com>
2020-10-21 22:31:08 -0400

    swift package init --type=executable

---
+++ README.md
@@ -0,0 +1,3 @@
+# plcrashreporter-spm-repro
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import plcrashreporter_spm_reproTests
+
+var tests = [XCTestCaseEntry]()
+tests += plcrashreporter_spm_reproTests.allTests()
+XCTMain(tests)
